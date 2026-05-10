b5a486a74ebbcd94a3e1cd93b16dc437a6a596d6
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2026-05-11 01:17:17 +0300

    test: add spec for existing path check

--- spec/joomla_download_spec.sh
+++ spec/joomla_download_spec.sh
@@ -1,13 +1,21 @@
 Describe 'joomla-download'
   It 'shows help message for --help'
-    When run script "joomla-download" "--help"
+    When run "./joomla-download" "--help"
     The status should be success
     The output should include "Usage: joomla-download <path> [<version>]"
   End
 
   It 'shows help message for -h'
-    When run script "joomla-download" "-h"
+    When run "./joomla-download" "-h"
     The status should be success
     The output should include "Usage: joomla-download <path> [<version>]"
+  End
+
+  Context 'when destination path does not exist'
+    It 'aborts with an error'
+      When run "./joomla-download" "/no/such/dir"
+      The status should be failure
+      The error should eq "joomla-download: path does not exist: /no/such/dir"
+    End
   End
 End
