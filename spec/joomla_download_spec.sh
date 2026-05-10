f7eb660233a7bfa70466c6ecc6a4ad8d46398094
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2026-05-11 01:07:26 +0300

    test: add spec for -h option

--- spec/joomla_download_spec.sh
+++ spec/joomla_download_spec.sh
@@ -1,9 +1,13 @@
 Describe 'joomla-download'
-  Describe '--help option'
-    It 'shows help message and exits with status 0'
-      When run script "joomla-download" "--help"
-      The status should be success
-      The output should include "Usage: joomla-download <path> [<version>]"
-    End
+  It 'shows help message for --help'
+    When run script "joomla-download" "--help"
+    The status should be success
+    The output should include "Usage: joomla-download <path> [<version>]"
+  End
+
+  It 'shows help message for -h'
+    When run script "joomla-download" "-h"
+    The status should be success
+    The output should include "Usage: joomla-download <path> [<version>]"
   End
 End
